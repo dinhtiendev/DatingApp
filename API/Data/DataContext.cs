114d380408ab265042e5de4ce8f3f3b183426c60
admin <dinhvantiendev@gmail.com>
2024-06-20 23:36:29 +0700

    feat: create API project

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class DataContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<AppUser> Users { get; set; }
+}
